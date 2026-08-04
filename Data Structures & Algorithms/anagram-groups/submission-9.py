f3ece770487d2aace8943dbc3382bb958ca7cac9
Geronimo Murillo <oscarone2002@gmail.com>
2026-08-03 22:33:34 -0600

    Add: anagram-groups - submission-9

---
+++ Data Structures & Algorithms/anagram-groups/submission-9.py
@@ -0,0 +1,12 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        diccionario = {}
+
+        for palabra in strs:
+            llave = "".join(sorted(palabra))
+            if llave in diccionario:
+                diccionario[llave].append(palabra)
+            else:
+                diccionario[llave] = [palabra]
+
+        return list(diccionario.values())
